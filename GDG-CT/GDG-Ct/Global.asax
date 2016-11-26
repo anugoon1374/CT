5232a96730d5f4a27a7cfe5949eff333d605da2f
anugoonl <a.lee@beinspire.is>
2016-11-26 12:39:25 +0700

    Setup project

---
+++ GDG-CT/GDG-Ct/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GDG_Ct.MvcApplication" Language="C#" %>
---
+++ GDG-CT/GDG-Ct/ViewModels/RequestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Beyond.Ct.Web.ViewModels
+{
+    public class RequestViewModel
+    {
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(120, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Context { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public List<string> ReceiverEmail { get; set; } = new List<string>();
+
+        [Required]
+        public List<string> ReceiverName { get; set; } = new List<string>();
+
+        [Required]
+        [EmailAddress]
+        public string RequesterEmail { get; set; }
+    }
+}
